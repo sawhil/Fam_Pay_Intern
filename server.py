b84bdf134b7e5a94d13839bc60b025c1ac341eb4
Sahil Raza Khan <sawhil.khan@gmail.com>
2021-04-03 11:38:01 +0530

    Implemented The DataBase Model

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # Function to Call The Youtube API
 # And Fetch The Data
@@ -17,8 +18,9 @@ def API():
 		'key' : YOUTUBE_API_KEY,
 		'q' : 'football',
 		'part' :'snippet',
-		'maxResults' : 5,
+		'maxResults' : 25,
 		'order' : 'date',
+		'type' : 'video',
 		'publishedAfter' : '2021-01-01T00:00:00Z'
 	}
 
@@ -31,13 +33,25 @@ def API():
 
 # Creating a Flask App Object
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///videos.db'
+db = SQLAlchemy(app)
 
+# Video title, description, publishing datetime, thumbnails URLs
+class Video(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	title = db.Column(db.String(1000), nullable = False)
+	description = db.Column(db.String(5000), nullable = True)
+	publishing_time = db.Column(db.DateTime)
+
+	def __repr__(self):
+		return f'<id = {self.id}, title = {self.title}, publishing_time = {self.publishing_time}>'
 
 
 # Decorator for Routing Initial GET Request
 @app.route('/', methods = ['GET'])
 def index():
-	return "Hello This will be the GET API For the Server"
+	return jsonify(API())
+	# return "Hello This will be the GET API For the Server"
 
 
 # Decorator for Routing Search Request
