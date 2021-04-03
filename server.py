14aa012bd8384410ad5e03f013d887a588c417c7
Sahil Raza Khan <sawhil.khan@gmail.com>
2021-04-03 14:12:09 +0530

    Created A DataBase and added all the videos to it

--- server.py
+++ server.py
@@ -1,34 +1,8 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-# Function to Call The Youtube API
-# And Fetch The Data
-
-def API():
-	# Youtube API Key
-	YOUTUBE_API_KEY = 'AIzaSyA9lbI0zPuK6DzhK-i4rsKgv0-y-27V60M'
-	
-	# Youtube Search URL to send the Request
-	search_url = 'https://www.googleapis.com/youtube/v3/search'
-	
-	# Parameters to Be Passed with the Request
-	search_params = {
-		'key' : YOUTUBE_API_KEY,
-		'q' : 'football',
-		'part' :'snippet',
-		'maxResults' : 25,
-		'order' : 'date',
-		'type' : 'video',
-		'publishedAfter' : '2021-01-01T00:00:00Z'
-	}
-
-	# Making the Request
-	r = requests.get(search_url, params = search_params)
-	
-	# Returning the Response Data
-	return r.json()
 
 
 # Creating a Flask App Object
@@ -40,25 +14,67 @@ db = SQLAlchemy(app)
 class Video(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	title = db.Column(db.String(1000), nullable = False)
-	description = db.Column(db.String(5000), nullable = True)
+	description = db.Column(db.String(5000))
+	thumbnail_url = db.Column(db.String(1000), nullable = False)
 	publishing_time = db.Column(db.DateTime)
-
 	def __repr__(self):
 		return f'<id = {self.id}, title = {self.title}, publishing_time = {self.publishing_time}>'
+
+
+
+# Function to Call The Youtube API
+# And Fetch The Data
+def YT_API():
+	# Youtube API Key
+	YOUTUBE_API_KEY = 'AIzaSyA9lbI0zPuK6DzhK-i4rsKgv0-y-27V60M'
+	
+	# Youtube Search URL to send the Request
+	search_url = 'https://www.googleapis.com/youtube/v3/search'
+	time_now = datetime.now()
+	last_req_time = time_now - timedelta(seconds=30)
+	req_time = last_req_time.replace(microsecond=0).isoformat()+'Z'
+	# Parameters to Be Passed with the Request
+	search_params = {
+		'key' : YOUTUBE_API_KEY,
+		'q' : 'cricket',
+		'part' :'snippet',
+		'maxResults' : 5,
+		'order' : 'date',
+		'type' : 'video',
+		'publishedAfter' : req_time
+	}
+
+	# Making the Request
+	r = requests.get(search_url, params = search_params)
+	results = r.json()['items']
+
+	for result in results:
+		video_title = result['snippet']['title']
+		video_description = result['snippet']['description']
+		video_thumbnail_url = result['snippet']['thumbnails']['medium']['url']
+		video_publishing_time = result['snippet']['publishedAt']
+		video_publishing_time = datetime.strptime(video_publishing_time, '%Y-%d-%mT%I:%M:%SZ')
+		new_video = Video(title = video_title, description = video_description, thumbnail_url = video_thumbnail_url, publishing_time = video_publishing_time)
+		try:
+			db.session.add(new_video)
+			db.session.commit()
+		except:
+			print('There was an Issue in Adding Video to the DataBase')
 
 
 # Decorator for Routing Initial GET Request
 @app.route('/', methods = ['GET'])
 def index():
-	return jsonify(API())
-	# return "Hello This will be the GET API For the Server"
+	videos = Video.query.order_by(Video.publishing_time.desc()).all()
+	return "Hello This will be the GET API For the Server"
 
 
 # Decorator for Routing Search Request
-@app.route('/search', methods = ['GET'])
+@app.route('/search', methods = ['POST'])
 def search():
 	return "This will return all the vids based on the search query"
 
 if __name__ == "__main__":
+	YT_API()
 	app.run(debug = True)
 
